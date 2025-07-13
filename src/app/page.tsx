4892a34ab7d99b28a19279f45b6cd6bb261544e5
adexfit <y.adeboye@gmail.com>
2025-07-13 14:19:04 +0100

    added dummy product on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+import products from "./data/products";
 
-export default function Home() {
+type ProductType = {
+  image: StaticImageData;
+  name: string;
+  price: number;
+};
+
+const Home = ({ image, name, price }: ProductType) => {
   return (
     <div>
       <main>
         <div> Catalogue</div>
+        <div>
+          {products.map((product) => {
+            return (
+              <div key={product.name}>
+                <Image
+                  priority
+                  src={product.image}
+                  alt={product.name}
+                  className="w-auto"
+                />
+                <li>{product.name}</li>
+                <p>{product.price}</p>
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
